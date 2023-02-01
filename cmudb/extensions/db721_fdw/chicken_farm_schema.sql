92f96017c38627236060a49c655083c60a968854
Wan Shen Lim <wanshen.lim@gmail.com>
2023-01-31 19:03:22 -0500

    S23: fix column name consistency.

--- cmudb/extensions/db721_fdw/chicken_farm_schema.sql
+++ cmudb/extensions/db721_fdw/chicken_farm_schema.sql
@@ -2,18 +2,18 @@ CREATE TABLE IF NOT EXISTS farm
 (
     farm_name       varchar,
 --     sexes         varchar[],
-    min_age_weeks   float,
-    max_age_weeks   float
+    min_age_weeks   real,
+    max_age_weeks   real
 );
 
 CREATE TABLE IF NOT EXISTS chicken
 (
-    identifier      int,
+    identifier      integer,
     farm_name       varchar,
     weight_model    varchar,
     sex             varchar,
-    age_weeks       float,
-    weight_g        float,
+    age_weeks       real,
+    weight_g        real,
     notes           varchar
 );
 
@@ -26,20 +26,20 @@ CREATE FOREIGN TABLE IF NOT EXISTS db721_farm
 (
     farm_name       varchar,
 --     sexes           varchar[],
-    min_age_weeks   float,
-    max_age_weeks   float
+    min_age_weeks   real,
+    max_age_weeks   real
 ) SERVER db721_server OPTIONS
 (
     filename '/home/kapi/git/postgres/data-farms.db721',
     tablename 'Farm'
 );
 CREATE FOREIGN TABLE IF NOT EXISTS db721_chicken (
-    identifier      int,
+    identifier      integer,
     farm_name       varchar,
     weight_model    varchar,
     sex             varchar,
-    age_weeks       float,
-    weight_g        float,
+    age_weeks       real,
+    weight_g        real,
     notes           varchar
 ) SERVER db721_server OPTIONS
 (
