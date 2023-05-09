14b428e85fedd4d1ade7e57c17c0e495268c5a09
xinyi7 <xinyi.jiang2@gmail.com>
2023-05-09 12:22:01 -0400

    deleted extension

--- cmudb/extensions/db721_fdw/db721_fdw--0.0.1.sql
+++
@@ -1,9 +0,0 @@
-\echo Use "CREATE EXTENSION db721_fdw" to load this file. \quit
-
-CREATE FUNCTION db721_fdw_handler()
-    RETURNS fdw_handler
-    AS 'MODULE_PATHNAME'
-    LANGUAGE C STRICT;
-
-CREATE FOREIGN DATA WRAPPER db721_fdw
-    HANDLER db721_fdw_handler;
--- cmudb/extensions/db721_fdw/src/dog.cpp
+++
@@ -1,5 +0,0 @@
-#include "dog.h"
-
-Dog::Dog(std::string name) : name_(name) {}
-
-std::string Dog::Bark() { return name_ + ": Woof!"; }
